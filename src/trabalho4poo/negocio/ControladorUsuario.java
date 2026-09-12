c90ef79aa76c5d32f21084dca523fb2c0ed8de8d
Lorena Alves Almeida <lorena.alves.almeida.lo@gmail.com>
2026-09-12 19:53:12 -0300

    ControladorUsuario

--- src/trabalho4poo/negocio/ControladorUsuario.java
+++ src/trabalho4poo/negocio/ControladorUsuario.java
@@ -1,6 +1,33 @@
 package trabalho4poo.negocio;
 
+import java.util.List;
+
+import trabalho4poo.dados.RepositorioUsuario;
 import trabalho4poo.ui.*;
+
 public class ControladorUsuario {
+	private RepositorioUsuario repoUsuario;
+
+	public ControladorUsuario() {
+		repoUsuario = new RepositorioUsuario();
+	}
+
+	public boolean add(Usuario u) {
+		if (u!= null && !repoUsuario.existeNome(u.getNmUsuario())) {
+			return repoUsuario.add(u);
+		} else {
+			return false;
+		}
+	}
+
+	//procurar o projeto por codigo
+	public Usuario projetoPorCod(int codigo) {
+		return repoUsuario.buscarPorCodigo(codigo);
+	}
+
+	// listar
+	public List<Usuario> listar() {
+		return repoUsuario.listar();
+	}
 
 }
